0a240f343349ae63f9c029a130b203d353617839
David Thorpe <djt@mutablelogic.com>
2024-04-10 11:28:22 +0200

    Updated

--- wc/core/event.js
+++
@@ -1,9 +0,0 @@
-// Define events used by web components
-const EVENT_CLICK = 'wc-click';
-const EVENT_START = 'wc-start';
-const EVENT_DONE = 'wc-done';
-const EVENT_ERROR = 'wc-error';
-
-export default {
-  EVENT_CLICK, EVENT_START, EVENT_DONE, EVENT_ERROR,
-};
